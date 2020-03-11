4e2f8a8cbad31db6d0f95a16274157da1e726c07
caowenhao <caowenhao@yiwise.com>
2020-03-11 16:35:05 +0800

    FIX 使用一知asr的sdk

--- app/src/main/java/com/yiwise/asr/demo/App.java
+++ app/src/main/java/com/yiwise/asr/demo/App.java
@@ -3,9 +3,6 @@ package com.yiwise.asr.demo;
 import android.app.Application;
 import android.content.Context;
 
-import com.richie.easylog.LoggerConfig;
-import com.richie.easylog.LoggerFactory;
-
 
 public class App extends Application {
     private static Context mContext;
@@ -20,11 +17,5 @@ public class App extends Application {
         super.onCreate();
 
         mContext = this;
-
-        // Debug 模式才开启日志
-        LoggerFactory.init(new LoggerConfig.Builder().context(this)
-                .logcatEnabled(true)
-                .logFileEnabled(true)
-                .build());
     }
 }
--- yiwise-asr-sdk/src/main/java/com/yiwise/asr/common/client/log/MdcLog.java
+++
@@ -1,5 +0,0 @@
-package com.yiwise.asr.common.client.log;
-
-public class MdcLog {
-    public static final String keyMdc = "MDC_LOG_ID";
-}
